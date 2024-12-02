5bf35621388503a4c8d8294c160d1e477e886cf3
Domski21 <dominator7331@gmail.com>
2024-12-02 12:58:07 +0100

    feat: add create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -6,3 +6,10 @@ def hello(user:str)->None:
 def read_users(users:list)->None:
     for user in users[1:]:
         print(f'Twój znajomy  {user['name']},z miasta {user['city']} opublikował {user['posts']} postów')
+
+def add_user(userlist:list) -> None:
+    new_name:str=input('Proszę podać imię nowego uźytkownika ')
+    new_posts:int=int(input('podaj liczbę postów '))
+    new_city:str=input('Podaj miasto ')
+    new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
+    userlist.append(new_user)
